75ad1a22cd755a478840447fd8b3159b7296a2a6
alewa <alewa8132@gmail.com>
2025-05-28 16:58:54 +0300

    Privilege class functionality implemented

--- ServManageSyst/GameEntitiesLib/Privilege.cpp
+++ ServManageSyst/GameEntitiesLib/Privilege.cpp
@@ -1,2 +1,76 @@
 // Copyright 2025 Alewa8131
 #include "Privilege.h"
+#include <string>
+#include <iostream>
+
+Privilege::Privilege()
+    : _name("None"), _price(0.0), _purchase_date(DateTime::DateTime()) {}
+
+Privilege::Privilege(const std::string& name, double price, const DateTime& purchase_date)
+    : _name(name), _price(price), _purchase_date(purchase_date) {}
+
+void Privilege::set_name(const std::string& name) {
+    _name = name;
+}
+void Privilege::set_price(double price) {
+    _price = price;
+}
+void Privilege::set_purchase_date(const DateTime& date) {
+    _purchase_date = date;
+}
+
+std::string Privilege::get_name() const {
+    return _name;
+}
+double Privilege::get_price() const {
+    return _price;
+}
+DateTime Privilege::get_purchase_date() const {
+    return _purchase_date;
+}
+
+int Privilege::get_duration_days() const {
+    DateTime now;
+    now.set_current();
+
+    int days_now = now.days_since_epoch();
+    int days_then = _purchase_date.days_since_epoch();
+
+    return days_now - days_then;
+}
+
+bool Privilege::is_active(int valid_days, const DateTime& now) const {
+    int purchase_days = _purchase_date.days_since_epoch();
+    int current_days = now.days_since_epoch();
+
+    return (current_days - purchase_days) < valid_days;
+}
+DateTime Privilege::get_expiry_date(int valid_days) {
+    DateTime copy = _purchase_date;
+    copy.add_days(valid_days);
+    return copy;
+}
+
+std::string Privilege::to_csv_line() const {
+    return _name + "," + std::to_string(_price) + "," + _purchase_date.to_string();
+}
+Privilege Privilege::from_csv_line(const std::string& line) {
+    size_t pos1 = line.find(',');
+    if (pos1 == std::string::npos) {
+        throw std::invalid_argument("Invalid CSV line: missing first comma");
+    }
+
+    size_t pos2 = line.find(',', pos1 + 1);
+    if (pos2 == std::string::npos) {
+        throw std::invalid_argument("Invalid CSV line: missing second comma");
+    }
+
+    std::string name = line.substr(0, pos1);
+    std::string price_str = line.substr(pos1 + 1, pos2 - pos1 - 1);
+    std::string date_str = line.substr(pos2 + 1);
+
+    double price = std::stod(price_str);
+    DateTime date = DateTime::from_string(date_str);
+
+    return Privilege(name, price, date);
+}
--- ServManageSyst/GameEntitiesLib/Privilege.h
+++ ServManageSyst/GameEntitiesLib/Privilege.h
@@ -7,11 +7,24 @@ class Privilege {
  private:
     std::string _name;
     double _price;
-    DateTime _purchaseDate;
-    int _durationDays;
+    DateTime _purchase_date;
 
  public:
-    void setName(const std::string& name);
-    void setPrice(double price);
-    void printInfo() const;
+    Privilege();
+    Privilege(const std::string& name, double price, const DateTime& purchase_date);
+
+    void set_name(const std::string& name);
+    void set_price(double price);
+    void set_purchase_date(const DateTime& date);
+
+    std::string get_name() const;
+    double get_price() const;
+    DateTime get_purchase_date() const;
+    int get_duration_days() const;
+
+    bool is_active(int valid_days, const DateTime& now) const;
+    DateTime get_expiry_date(int valid_days);
+
+    std::string to_csv_line() const;
+    static Privilege from_csv_line(const std::string& line);
 };
